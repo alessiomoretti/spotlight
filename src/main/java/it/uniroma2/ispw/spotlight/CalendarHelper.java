958bb5cdd06dbe86cbd1fe21d7cc034fc1119e85
Alessio Moretti <alessio.moretti@live.it>
2018-04-21 20:26:47 +0200

    Add calendar helper utility.

---
+++ src/main/java/it/uniroma2/ispw/spotlight/CalendarHelper.java
@@ -0,0 +1,23 @@
+package it.uniroma2.ispw.spotlight;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarHelper {
+
+    public static Date getDate(Integer day, Integer month, Integer year, Integer hour, Integer minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        return calendar.getTime();
+    }
+
+    public static Date getToday() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        return calendar.getTime();
+    }
+}
