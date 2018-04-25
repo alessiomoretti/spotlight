9fdae76986cb680251b4243985db8c240dbccc16
Alessio Moretti <alessio.moretti@live.it>
2018-04-25 17:10:25 +0200

    Improve calendar helper (no seconds and milliseconds retrieving date)

--- src/main/java/it/uniroma2/ispw/spotlight/CalendarHelper.java
+++ src/main/java/it/uniroma2/ispw/spotlight/CalendarHelper.java
@@ -12,12 +12,15 @@ public class CalendarHelper {
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
     }
 
     public static Date getToday() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.SECOND, 0);
         return calendar.getTime();
     }
 }
