e501ea3715853462347a7df272c8834230c30ad4
Alessio Moretti <alessio.moretti@live.it>
2018-04-27 22:41:13 +0200

    Fix calendar helper - now the 00.00.0000 hour, minute and millisecond is selected on today retrieval

--- src/main/java/it/uniroma2/ispw/spotlight/helpers/CalendarHelper.java
+++ src/main/java/it/uniroma2/ispw/spotlight/helpers/CalendarHelper.java
@@ -20,7 +20,9 @@ public class CalendarHelper {
     public static Date getToday() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
     }
 }
